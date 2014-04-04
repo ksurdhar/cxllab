8c5b95eb5d3de2d034749b09138576c3ecdaeedc
Kiran Surdhar <ksurdhar1@gmail.com>
2014-04-03 22:44:58 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cxllab::Application.config.secret_key_base = 'ad19dc58bf992d78b8c383b9c83072b2ee93c25acde7fbf0ded1d269cbdbc3843640cb146f0832f2db4823c49753737022cf788dc182bfd2ae9fb2e880b1b735'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cxllab::Application.config.session_store :cookie_store, key: '_cxllab_session'
