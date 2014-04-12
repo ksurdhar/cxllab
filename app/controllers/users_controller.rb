97707b36326e372322675d662ae9ed8e11ce4bc0
Kiran Surdhar <ksurdhar1@gmail.com>
2014-04-11 20:04:56 -0700

    added new user controller to override devise defaults

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,16 @@ class UsersController < ApplicationController
     client = Soundcloud.new(:client_id => '5a1ab580242d18027f496e01bfc31064',
                         :client_secret => '62ab42202db997a3855d6e7d8d5c67db',
                         :redirect_uri => new_user_registration_url)
-
     redirect_to client.authorize_url()
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :about, :genre, :producer, :sc_access_token, :sc_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Cxllab::Application.routes.draw do
   root to: "site#root"
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
 
-  resources :users, only: [] do
+  resources :users, only: [:show] do
     collection do
       get 'sc_connect'
     end
