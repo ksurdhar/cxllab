a99ad1eb3516c5a13f9013c5616f924072404246
Kiran Surdhar <ksurdhar1@gmail.com>
2014-04-17 15:17:14 -0700

    made sure that we don't include ourselves in the user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class Api::UsersController < ApplicationController
   end
 
   def index
-    @users = User.all - current_user.liked_users
+    @users = User.all - current_user.liked_users - current_user
     render "users/index"
   end
 
