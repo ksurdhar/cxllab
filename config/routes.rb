5dfefa379e6d83c1ac9a81e7c0beaee3648d8c6f
Kiran Surdhar <ksurdhar1@gmail.com>
2014-04-18 13:50:41 -0700

    reverted order of soundcloud route

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Cxllab::Application.routes.draw do
   root to: "site#root"
   devise_for :users, controllers: { registrations: "registrations" }
 
-  namespace :api, :defaults => { :format => :json } do
-    resources :users, only: [:index, :show]
-    resources :relationships, only: [:create, :index]
-  end
-
   namespace :api do
     resources :users, only: [] do
       collection do
@@ -14,5 +9,12 @@ Cxllab::Application.routes.draw do
       end
     end
   end
+  
+  namespace :api, :defaults => { :format => :json } do
+    resources :users, only: [:index, :show]
+    resources :relationships, only: [:create, :index]
+  end
+
+  
 
 end
