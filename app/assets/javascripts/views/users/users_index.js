2c7d97e49b1a90035202ca02a4401417bcfdc649
Kiran Surdhar <ksurdhar1@gmail.com>
2014-04-19 14:26:04 -0700

    soundcloud player is unique to user, added dislike button

--- app/assets/javascripts/views/users/users_index.js
+++ app/assets/javascripts/views/users/users_index.js
@@ -5,26 +5,27 @@ Cxllab.Views.usersIndex = Backbone.View.extend({
   initialize: function (options){
     this.listenTo(this.collection, "sync change remove", this.render);
     SC.initialize({ client_id: '5a1ab580242d18027f496e01bfc31064' });
-
   },
 
   events:{
-    'click #newRelationBtn': 'createLike'
+    'click #newLikeBtn': 'createLike',
+    'click #newHateBtn': 'createHate'
   },
 
   render: function(){
-    var renderedContent = this.template({ users: this.collection });
+    var next_user = this.nextUser(this.collection);
+    
+    var renderedContent = this.template({ user: next_user });
     this.$el.html(renderedContent);
-    setTimeout(this.renderPlayer, 250);
+    setTimeout(this.renderPlayer(next_user), 250);
     return this;
   },
 
   createLike: function(e){
     var liked_id = $(e.currentTarget).data("id")
-    var current_user_id = Cxllab.current_user.get("id");
 
     var like = new Cxllab.Models.Relationship({
-      liker_id: current_user_id,
+      liker_id: global_user_id,
       liked_user_id: liked_id,
       like: true
     });
@@ -35,19 +36,40 @@ Cxllab.Views.usersIndex = Backbone.View.extend({
     like.save();
   },
 
-  _counter: function(){
-    if(!this._counterNum){
-      this._counterNum = 0
-    }
-    return this._counterNum;
+  createHate: function(e){
+    var liked_id = $(e.currentTarget).data("id")
+
+    var like = new Cxllab.Models.Relationship({
+      liker_id: global_user_id,
+      liked_user_id: liked_id
+    });
+
+    var liked_user = Cxllab.Collections.my_users.where({id: liked_id})
+    Cxllab.Collections.my_users.remove(liked_user)
+
+    like.save();
   },
 
-  renderPlayer: function(){
+  renderPlayer: function(user){
+    if(user){
+      var username = user.get("sc_username")
+      var track_url = 'http://soundcloud.com/' + username;
 
-    var track_url = 'http://soundcloud.com/mynameisezra';
-    SC.oEmbed(track_url, {auto_play: false, show_comments: false, 
+      SC.oEmbed(track_url, {auto_play: false, show_comments: false, 
       maxheight: 166, sharing: false, buying: false, download: false}, 
       document.getElementById('player'));
+    }
+  },
+
+  nextUser: function(collection){
+    if(this.collection.length > 0){ 
+      var id = this.collection.length
+      if(id === global_user_id){
+        id++
+      } 
+      var next_user = this.collection.get({id: id})
+      return next_user
+    }
   }
 
 });
