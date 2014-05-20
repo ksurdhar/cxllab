b778eee1e2b025cb79c97489a3d9a95d24b10f1c
Kiran Surdhar <ksurdhar1@gmail.com>
2014-05-20 13:32:05 -0400

    updating the new user redirect URI

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ class Api::UsersController < ApplicationController
   def sc_connect
     client = Soundcloud.new(:client_id => '5a1ab580242d18027f496e01bfc31064',
                         :client_secret => '62ab42202db997a3855d6e7d8d5c67db',
-                        :redirect_uri => new_user_registration_url, :scope => "non-expiring")
+                        :redirect_uri => 'https://cxllab.herokuapp.com/users/sign_up', :scope => "non-expiring")
     redirect_to client.authorize_url()
   end
 
